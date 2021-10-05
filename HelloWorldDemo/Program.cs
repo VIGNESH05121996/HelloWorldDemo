68df56d6da1ca731ab0303b7fba6c7ba1acd49e2
VIGNESH05121996 <90402981+VIGNESH05121996@users.noreply.github.com>
2021-10-05 12:19:31 +0530

    Pull Command Demo

--- HelloWorldDemo/Program.cs
+++ HelloWorldDemo/Program.cs
@@ -12,6 +12,11 @@ namespace HelloWorldDemo
         public static void Addition()
         {
             Console.WriteLine("Addition Operation")
+            Console.WriteLine("Merging Changes With Master")
+            Console.WriteLine("Modifying in remote master")
+            Console.WriteLine("Creating conflict at local")
+            Console.WriteLine("Creating Conflict at line no 23")
+            Console.WriteLine("Testing Merge")    _    
         }
     }
 }
